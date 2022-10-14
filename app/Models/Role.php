eca7a3717ec699e29d88beb43eba583ffa27deee
imamfchrj <imam.fchrj@gmail.com>
2022-10-14 19:28:52 +0700

    Munculkan menu setting
    Munculkan menu setting

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -24,7 +24,7 @@ class Role extends SpatieRole
 //    protected $keyType = 'string';
 //    public $incrementing = false;
 
-    const ADMIN = 'Admin';
+    const ADMIN = 'Superadmin';
 
     public function getCreatedAtFormattedAttribute()
     {
