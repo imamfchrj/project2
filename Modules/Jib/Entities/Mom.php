e4dab495ab55864e6b9a7780ec37c6d92062a580
imamfchrj <imam.fchrj@gmail.com>
2022-10-23 08:54:05 +0700

    create entities mom
    create entities mom

--- Modules/Jib/Entities/Mom.php
+++ Modules/Jib/Entities/Mom.php
@@ -9,8 +9,38 @@ class Mom extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
-    
+    protected $fillable = [
+        'pengajuan_id',
+        'dasar_mom',
+        'ruang_lingkup',
+        'tanggal_mom',
+        'spesifikasi',
+        'venue',
+        'kegiatan',
+        'meeting_called',
+        'lokasi',
+        'meeting_type',
+        'top',
+        'facilitator',
+        'aki',
+        'attende',
+        'catatan',
+        'kelengkapan',
+        'anggaran',
+        'created_by',
+        'updated_by'
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+
+    protected $table = 'jib_mom';
+    protected $primaryKey = 'id';
+
+
     protected static function newFactory()
     {
         return \Modules\Jib\Database\factories\MomFactory::new();
